65ce18bb3063071cf5459d26fdb25f57de57f661
sonomoo <sbxjs010@naver.com>
2025-09-27 15:25:19 +0900

    first commit

---
+++ src/main/java/ErpProject/ErpProjectV3/ErpProjectV3Application.java
@@ -0,0 +1,13 @@
+package ErpProject.ErpProjectV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpProjectV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpProjectV3Application.class, args);
+	}
+
+}
