a10d8e741f7799c80c0c14d6d7b39fa004506d7e
sonomoo <sbxjs010@naver.com>
2025-11-03 01:48:45 +0900

    product CRUD

--- src/main/java/erpproject/worksin/domain/product/controller/ProductController.java
+++ src/main/java/erpproject/worksin/domain/product/controller/ProductController.java
@@ -1,15 +1,14 @@
 package erpproject.worksin.domain.product.controller;
 
+import erpproject.worksin.common.response.ApiResponse;
 import erpproject.worksin.domain.product.dto.request.ProductRequest;
 import erpproject.worksin.domain.product.dto.response.ProductResponse;
 import erpproject.worksin.domain.product.service.ProductService;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -22,5 +21,27 @@ public class ProductController {
     public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
 
         return productService.createProduct(productRequest);
+    }
+
+    @GetMapping("/v1/product/{productId}")
+    public ResponseEntity<ApiResponse<ProductResponse>> getProduct(@PathVariable Long productId){
+
+        ProductResponse product = productService.getProduct(productId);
+
+        return ApiResponse.success(product);
+    }
+
+    @DeleteMapping("/v1/product/{productId}")
+    public ResponseEntity<ApiResponse<Void>> deleteProduct(@PathVariable Long productId){
+
+        return ApiResponse.noContent(productService.DeleteProduct(productId));
+    }
+
+    @PatchMapping("/v1/product/{productId}")
+    public ResponseEntity<ApiResponse<ProductResponse>> updateProduct(@PathVariable Long productId,
+                                                                      @RequestBody ProductRequest productRequest){
+
+        productService.updateProduct();
+
     }
 }
--- src/main/java/erpproject/worksin/domain/product/service/ProductService.java
+++ src/main/java/erpproject/worksin/domain/product/service/ProductService.java
@@ -1,11 +1,13 @@
 package erpproject.worksin.domain.product.service;
 
+import erpproject.worksin.common.response.ApiResponse;
 import erpproject.worksin.domain.product.dto.request.ProductRequest;
 import erpproject.worksin.domain.product.dto.response.ProductResponse;
 import erpproject.worksin.domain.product.entity.Product;
 import erpproject.worksin.domain.product.repository.ProductRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductFinder productFinder;
 
     public ProductResponse createProduct(ProductRequest productRequest){
 
@@ -27,6 +30,33 @@ public class ProductService {
         productRepository.save(product);
 
         return ProductResponse.from(product);
+    }
+
+    public Void DeleteProduct(Long productId){
+
+        return productRepository.delete(productId);
+    }
+
+    public ProductResponse getProduct(Long productId) {
+        Product productById = productFinder.getProductById(productId);
+
+        return ProductResponse.from(productById);
+    }
+
+    public ProductResponse updateProduct(Long productId, ProductRequest productRequest) {
+        Product productById = productFinder.getProductById(productId);
+
+        Product product = Product.of(productRequest.brand(),
+                productRequest.cereal(),
+                productRequest.explain(),
+                productRequest.model_info(),
+                productRequest.name(),
+                productRequest.price(),
+                productRequest.size());
+
+        return ProductResponse.from(product);
+
 
     }
+
 }
